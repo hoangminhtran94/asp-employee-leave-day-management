eff2af5c2011278fc2f257bfdb9a249ae9db3d4a
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-05-23 14:47:43 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace first_asp_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
