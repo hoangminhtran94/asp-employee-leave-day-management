adc67e74d0f4d78f58083dc03eb16a492298f863
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-05-24 12:38:46 -0400

    add leave table, employee table, leave allocation, migrations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace first_asp_app.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     }
 }
---
+++ Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace first_asp_app.Data
+{
+    public partial class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
